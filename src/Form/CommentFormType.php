81f8e166cdf4f46864960c758312f280e5dcdf9e
Sidney Mangoye <mangoyesidney@gmail.com>
2021-07-16 16:17:19 +0200

    add comment form

---
+++ src/Form/CommentFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\Core\Type\EmailType;
+use Symfony\Component\Form\Exception\Core\Type\FileType;
+use Symfony\Component\Form\Exception\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType as TypeEmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author', null, [
+                'label' => 'Your name',
+            ])
+            ->add('text')
+            ->add('email', EmailType::class)
+            ->add('photo', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new Image(['maxSize' => '1024'])
+                ],
+            ])
+            ->add('submit', SUbmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
